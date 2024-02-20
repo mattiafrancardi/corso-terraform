9bcb917e4d056d77b5c49cb6d45b09b605bac020
mattiafrancardi <mattia.francardi@mauden.com>
2024-02-20 15:42:38 +0100

    removed cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "corso-terraform"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
